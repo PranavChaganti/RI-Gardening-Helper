83a34fc9fb88cc2afcdd579aeb6e6edb25c1140d
PranavChaganti <pran.chag08@gmail.com>
2025-06-14 11:26:26 -0700

    Update RIGH-bot.py
    Modified for getenv

--- RIGH-bot.py
+++ RIGH-bot.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request, jsonify, Response
 import getpass
 import os
 import openai
-os.environ['OPENAI_API_KEY'] = getpass.getpass("OpenAI API Key: ")
-#openai.api_key = 'MY-OPENAI-KEY'
+#os.environ['OPENAI_API_KEY'] = getpass.getpass("OpenAI API Key: ")
+openai.api_key = os.getenv('MY-OPENAI-KEY')
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 import nest_asyncio
 
@@ -13,8 +13,8 @@ nest_asyncio.apply()
 import getpass
 import os
 
-os.environ["LLAMA_CLOUD_API_KEY"] = getpass.getpass()
-#LLAMA_CLOUD_API_KEY = os.getenv('LLAMA_CLOUD_API_KEY')
+#os.environ["LLAMA_CLOUD_API_KEY"] = getpass.getpass()
+LLAMA_CLOUD_API_KEY = os.getenv('LLAMA_CLOUD_API_KEY')
 #LLAMA_CLOUD_API_KEY='llx-FATx2gexPefGWiVotb8SHCIzHOy6QAIh0LZSSVP3IvZ8171K'
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from llama_parse import LlamaParse
