c59605d6588a5f11f30489cca070cb3854097885
PranavChaganti <pran.chag08@gmail.com>
2025-06-14 12:14:51 -0700

    Update RIGH-bot.py
    Added flask port

--- RIGH-bot.py
+++ RIGH-bot.py
@@ -22,3 +22,27 @@ from llama_parse import LlamaParse
 #documents = SimpleDirectoryReader("/Users/pc/aiml/ai-env/Data").load_data()
 documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+app = Flask(__name__)
+
+# Example chatbot function
+def chatbot_response(message):
+    response = query_engine.query(message)
+    return f"{response}"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    user_message = request.form["message"]
+    bot_reply = chatbot_response(user_message)
+    formatted_response = bot_reply.replace("\n", "<br>")
+    url_pattern = r'(https?://[^\s]+)'
+    formatted_response = re.sub(url_pattern, r'<a href="\1" target="_blank">\1</a>', formatted_response)
+    return jsonify({"response": formatted_response})
+
+if __name__ == "__main__":
+    #app.run(host="127.0.0.1",debug=True)
+    app.run(debug=True, port=5000)
